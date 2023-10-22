28b4120729a488688de661cdb1b6165b9daf9e08
RafaelLavatori <104533739+RafaelLavatori@users.noreply.github.com>
2023-10-22 15:18:10 -0300

    add mqtt conection with love

--- nodemcu/lovemqtt.lua
+++ nodemcu/lovemqtt.lua
@@ -1,10 +1,20 @@
--- id do conectado (battleship1 e battleship2)
-local meuid = "nodebattleship1"
-local m = mqtt.Client(meuid, 120)
+local chave = 0
+local delay = 200000
+local last = 0
+local sw1 = 3
+local sw2 = 4
+local sw3 = 5
+gpio.mode(sw1,gpio.INT,gpio.PULLUP)
+gpio.mode(sw2,gpio.INT,gpio.PULLUP)
+gpio.mode(sw3,gpio.INT,gpio.PULLUP)
 
-function publica(c)
-  c:publish("nodebattleship1","alo de " .. meuid,0,0, 
-            function(client) print("mandou!") end)
+
+local meuid = "nodebattleship1"
+local m = mqtt.Client("clientid " .. meuid, 120)
+
+function publica(c,chave)
+  c:publish("nodebattleship1",chave,0,0, 
+            function(client) print("mandou! "..chave) end)
 end
 
 function novaInscricao (c)
@@ -17,12 +27,30 @@ function novaInscricao (c)
 end
 
 function conectado (client)
-  publica(client)
   client:subscribe("lovebattleship1", 0, novaInscricao)
+  gpio.trig(sw1, "down", 
+    function (level,timestamp)
+        if timestamp - last < delay then return end
+        last = timestamp
+        chave = 1
+        publica(client,chave)
+    end)
+  gpio.trig(sw2, "down", 
+    function (level,timestamp)
+        if timestamp - last < delay then return end
+        last = timestamp
+        chave = 2
+        publica(client,chave)
+    end)
+  gpio.trig(sw3, "down", 
+    function (level,timestamp)
+        if timestamp - last < delay then return end
+        last = timestamp
+        chave = 3
+        publica(client,chave)
+    end)
 end 
 
 m:connect("139.82.100.100", 7981, false, 
              conectado,
              function(client, reason) print("failed reason: "..reason) end)
-
-
